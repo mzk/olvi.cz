cc685290f27f57a040ef05c9e59373bc2347deed
Oldrich Salek <mzk@mozektevidi.net>
2013-12-27 09:13:15 +0100

    added static templates, reformated to Texy

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -11,4 +11,13 @@ use Nette, Model;
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
 
+	protected function createTemplate($class = NULL)
+	{
+		$tpl = parent::createTemplate($class);
+
+		// registrace Texy helperu
+		$tpl->registerHelper('texy', callback(new \Texy(), 'process'));
+		return $tpl;
+	}
+
 }
